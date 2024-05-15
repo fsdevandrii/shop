1b523a01f90da4b239136d5391a1d0855999a4c2
fsdevandrii <135020138+fsdevandrii@users.noreply.github.com>
2024-05-15 22:30:14 +0300

    init

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Items from "./components/Items";
 import Categories from "./components/Categories";
+import ShowFullItem from "./components/ShowFullItem";
 
 class App extends React.Component {
   constructor(props) {
@@ -48,25 +49,51 @@ class App extends React.Component {
           price: "29.99",
         },
       ],
+      showFullItem: false,
+      fullItem: {},
     };
     this.state.currentItems = this.state.items;
     this.adToOrder = this.adToOrder.bind(this);
     this.deleteOrder = this.deleteOrder.bind(this);
     this.chooseCategory = this.chooseCategory.bind(this);
+    this.onShowItem = this.onShowItem.bind(this);
   }
   render() {
     return (
       <div className="wrapper">
         <Header orders={this.state.orders} onDelete={this.deleteOrder} />
         <Categories chooseCategory={this.chooseCategory} />
-        <Items items={this.state.items} onAdd={this.adToOrder} />
+        <Items
+          onShowItem={this.onShowItem}
+          items={this.state.currentItems}
+          onAdd={this.adToOrder}
+        />
+
+        {this.state.showFullItem && (
+          <ShowFullItem
+            onAdd={this.adToOrder}
+            onShowItem={this.onShowItem}
+            item={this.state.fullItem}
+          />
+        )}
         <Footer />
       </div>
     );
   }
 
+  onShowItem(item) {
+    this.setState({ fullItem: item });
+    this.setState({ showFullItem: !this.state.showFullItem });
+  }
+
   chooseCategory(category) {
-    console.log(category);
+    if (category === "all") {
+      this.setState({ currentItems: this.state.items });
+      return;
+    }
+    this.setState({
+      currentItems: this.state.items.filter((el) => el.category === category),
+    });
   }
 
   deleteOrder(id) {
--- src/components/Items.js
+++ src/components/Items.js
@@ -6,7 +6,12 @@ export class Items extends Component {
     return (
       <main>
         {this.props.items.map((el) => (
-          <Unit key={el.id} item={el} onAdd={this.props.onAdd} />
+          <Unit
+            onShowItem={this.props.onShowItem}
+            key={el.id}
+            item={el}
+            onAdd={this.props.onAdd}
+          />
         ))}
       </main>
     );
